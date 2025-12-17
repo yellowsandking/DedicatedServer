ec41a41e5a9f194325e48b77e5287e73b3af8297
yellowsandking <108459863+yellowsandking@users.noreply.github.com>
2025-12-17 15:43:13 +0800

    只cook某个map

---
+++ SeverTest/CookMap.bat
@@ -0,0 +1,9 @@
+G:\WorkSpace\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
+-project="G:\WorkSpace\DedicatedServer\SeverTest\SeverTest.uproject" ^
+-server ^
+-serverplatform=Win64 ^
+-cook ^
+-map=/Game/Maps/ServerMap ^
+-build ^
+-stage ^
+-package
