f7cf10051b3ce938fc0bf24ffa265ce62b27c332
yellowsandking <108459863+yellowsandking@users.noreply.github.com>
2025-12-17 15:28:45 +0800

    cook server批处理文件

---
+++ SeverTest/test.bat
@@ -0,0 +1,9 @@
+G:\WorkSpace\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
+-project="G:\WorkSpace\DedicatedServer\SeverTest\SeverTest.uproject" ^
+-server ^
+-serverplatform=Win64 ^
+-cook ^
+-clean ^
+-build ^
+-stage ^
+-package
